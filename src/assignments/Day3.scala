6457207f5cef7e8dd1876d010b3501b4761a5d9f
Borek Beker <borek.beker@nos.nl>
2022-02-04 16:04:21 +0100

    Small refactoring

--- src/assignments/Day3.scala
+++ src/assignments/Day3.scala
@@ -1,5 +1,7 @@
 package assignments
 
+import scala.annotation.tailrec
+
 object Day3_1 {
 
   private val RIGHT_COUNT = 3
@@ -8,28 +10,25 @@ object Day3_1 {
   def printAnswer(input: Seq[String]): Unit = println(countTrees(input.toList))
 
   private def countTrees(input: List[String], index: Int = 0, stepsRight: Int = 0, check: Boolean = false): Int = check match {
-    case true => {
-      getLine(input, index) match {
-        case null => 0
-        case line if hasTree(line.toList, line.length, stepsRight) => 1 + countTrees(input, index, stepsRight, false)
-        case _ => countTrees(input, index, stepsRight, false)
-      }
+    case true => getLine(input, index) match {
+      case null => 0
+      case line if hasTree(line.toList, line.length, stepsRight) => 1 + countTrees(input, index, stepsRight)
+      case _ => countTrees(input, index, stepsRight)
     }
-    case false => countTrees(input, index + 1, stepsRight + RIGHT_COUNT, true)
+    case false => countTrees(input, index + 1, stepsRight + RIGHT_COUNT, check = true)
   }
 
+  @tailrec
   private def getLine(input: List[String], index: Int, count: Int = 0): String = input match {
     case Nil => null
     case head :: _ if count == index => head
     case _ :: tail => getLine(tail, index, count + 1)
   }
 
-  // THIS WORKS
-  private def hasTree(line: List[Char], lineLength: Int, index: Int, current: Int = 0): Boolean = {
-    line match {
-      case Nil => false
-      case char :: _ if current == (index % lineLength) => char == TREE
-      case _ :: rest => hasTree(rest, lineLength, index, current + 1)
-    }
+  @tailrec
+  private def hasTree(line: List[Char], lineLength: Int, index: Int, current: Int = 0): Boolean = line match {
+    case Nil => false
+    case char :: _ if current == (index % lineLength) => char == TREE
+    case _ :: rest => hasTree(rest, lineLength, index, current + 1)
   }
 }
