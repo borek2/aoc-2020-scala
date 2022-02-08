2545744d8ba6b741cc7bd096da257be95d384ab5
Borek Beker <borek.beker@nos.nl>
2022-02-08 21:09:04 +0100

    Finished day 4 part 2

--- src/Main.scala
+++ src/Main.scala
@@ -2,7 +2,7 @@ import assignments.{Day4}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val testSource = scala.io.Source.fromFile("src/resources/Day4Test2")
+    val testSource = scala.io.Source.fromFile("src/resources/Day4")
     val testLines = testSource.getLines().toSeq
     Day4.printAnswer2(testLines)
     testSource.close()
--- src/assignments/Day4.scala
+++ src/assignments/Day4.scala
@@ -30,8 +30,8 @@ object Day4 {
 
   def countValidPassports2(input: List[String], neededEntries: List[String] = neededEntries): Int = input match {
     case Nil => neededEntries.isEmpty.toInt
-    case "" :: rest => neededEntries.isEmpty.toInt + countValidPassports(rest, Day4.neededEntries)
-    case line :: rest => countValidPassports(rest, filterNeededEntries(line.toList, neededEntries))
+    case "" :: rest => neededEntries.isEmpty.toInt + countValidPassports2(rest, Day4.neededEntries)
+    case line :: rest => countValidPassports2(rest, filterNeededEntries2(line.toList, neededEntries))
   }
 
   @tailrec
@@ -40,6 +40,68 @@ object Day4 {
     case char1 :: char2 :: char3 :: ':' :: rest =>
       filterNeededEntries(rest, neededEntries.filter(str => str != (char1 :: char2 :: char3 :: Nil).stringify))
     case _ :: rest => filterNeededEntries(rest, neededEntries)
+  }
+
+  def filterNeededEntries2(line: List[Char], neededEntries: List[String] = neededEntries): List[String] = line match {
+    case Nil => neededEntries
+    case char1 :: char2 :: char3 :: ':' :: rest if line.isValid =>
+      filterNeededEntries2(rest, neededEntries.filter(str => str != (char1 :: char2 :: char3 :: Nil).stringify))
+    case _ :: rest => filterNeededEntries2(rest, neededEntries)
+  }
+
+  /**
+   * byr (Birth Year) - four digits; at least 1920 and at most 2002.
+   * iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+   * eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+   * hgt (Height) - a number followed by either cm or in:
+   * If cm, the number must be at least 150 and at most 193.
+   * If in, the number must be at least 59 and at most 76.
+   * hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+   * ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+   * pid (Passport ID) - a nine-digit number, including leading zeroes.
+   * cid (Country ID) - ignored, missing or not.
+   */
+
+  implicit class asPasswordEntryValue(chars: List[Char]) {
+    def isValid: Boolean = chars match {
+      case 'b' :: 'y' :: 'r' :: ':' :: rest => are4DigitsBetween(1920, 2002, rest)
+      case 'i' :: 'y' :: 'r' :: ':' :: rest => are4DigitsBetween(2010, 2020, rest)
+      case 'e' :: 'y' :: 'r' :: ':' :: rest => are4DigitsBetween(2020, 2030, rest)
+      case 'h' :: 'g' :: 't' :: ':' :: rest => isValidHgt(rest)
+      case 'h' :: 'c' :: 'l' :: ':' :: rest => isValidHcl(rest)
+      case 'e' :: 'c' :: 'l' :: ':' :: rest => isValidEcl(rest)
+      case 'p' :: 'i' :: 'd' :: ':' :: rest => isValidPid(rest)
+      case _ => true
+    }
+
+    private def are4DigitsBetween(low: Int, high: Int, chars: List[Char]): Boolean = chars match {
+      case char1 :: char2 :: char3 :: char4 :: tail if tail == Nil || tail.head == ' ' =>
+        chars.take(4).forall(_.isDigit) && s"${char1}${char2}${char3}${char4}".toInt >= low && s"${char1}${char2}${char3}${char4}".toInt <= high
+      case _ => false
+    }
+
+    private def isValidHgt(chars: List[Char], currentNumberString: String = ""): Boolean = chars match {
+      case 'i' :: 'n' :: tail if tail == Nil || tail.head == ' ' && currentNumberString.nonEmpty => currentNumberString.toInt >= 59 && currentNumberString.toInt <= 76
+      case 'c' :: 'm' :: tail if tail == Nil || tail.head == ' ' && currentNumberString.nonEmpty => currentNumberString.toInt >= 150 && currentNumberString.toInt <= 193
+      case char :: rest if char.isDigit => isValidHgt(rest, s"$currentNumberString$char")
+      case _ => false
+    }
+
+    private def isValidHcl(chars: List[Char]): Boolean = chars match {
+      case '#' :: char1 :: char2 :: char3 :: char4 :: char5 :: char6 :: tail if tail == Nil || tail.head == ' ' =>
+        s"$char1$char2$char3$char4$char5$char6".matches("[0-9a-z]+")
+      case _ => false
+    }
+
+    private def isValidEcl(chars: List[Char]): Boolean = chars match {
+      case char1 :: char2 :: char3 :: tail if tail == Nil || tail.head == ' ' => s"$char1$char2$char3".matches("amb|blu|brn|gry|grn|hzl|oth")
+      case _ => false
+    }
+
+    private def isValidPid(chars: List[Char]): Boolean = chars match {
+      case _ :: _ :: _ :: _ :: _ :: _ :: _ :: _ :: _ :: tail if tail == Nil || tail.head == ' ' => chars.take(9).forall(_.isDigit)
+      case _ => false
+    }
   }
 
   implicit class asInt(b: Boolean) {
