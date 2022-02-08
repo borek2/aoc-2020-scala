877ff97f5b960a322e7b2a32256828737fb1f444
Borek Beker <borek.beker@nos.nl>
2022-02-08 21:13:18 +0100

    Some more updates

--- src/assignments/Day4.scala
+++ src/assignments/Day4.scala
@@ -2,16 +2,6 @@ package assignments
 
 import scala.annotation.tailrec
 
-/**
- * byr (Birth Year)
- * iyr (Issue Year)
- * eyr (Expiration Year)
- * hgt (Height)
- * hcl (Hair Color)
- * ecl (Eye Color)
- * pid (Passport ID)
- * cid (Country ID)
- */
 object Day4 {
 
   val neededEntries = List(
@@ -28,12 +18,6 @@ object Day4 {
     case line :: rest => countValidPassports(rest, filterNeededEntries(line.toList, neededEntries))
   }
 
-  def countValidPassports2(input: List[String], neededEntries: List[String] = neededEntries): Int = input match {
-    case Nil => neededEntries.isEmpty.toInt
-    case "" :: rest => neededEntries.isEmpty.toInt + countValidPassports2(rest, Day4.neededEntries)
-    case line :: rest => countValidPassports2(rest, filterNeededEntries2(line.toList, neededEntries))
-  }
-
   @tailrec
   def filterNeededEntries(line: List[Char], neededEntries: List[String] = neededEntries): List[String] = line match {
     case Nil => neededEntries
@@ -42,25 +26,19 @@ object Day4 {
     case _ :: rest => filterNeededEntries(rest, neededEntries)
   }
 
+  def countValidPassports2(input: List[String], neededEntries: List[String] = neededEntries): Int = input match {
+    case Nil => neededEntries.isEmpty.toInt
+    case "" :: rest => neededEntries.isEmpty.toInt + countValidPassports2(rest, Day4.neededEntries)
+    case line :: rest => countValidPassports2(rest, filterNeededEntries2(line.toList, neededEntries))
+  }
+
+  @tailrec
   def filterNeededEntries2(line: List[Char], neededEntries: List[String] = neededEntries): List[String] = line match {
     case Nil => neededEntries
     case char1 :: char2 :: char3 :: ':' :: rest if line.isValid =>
       filterNeededEntries2(rest, neededEntries.filter(str => str != (char1 :: char2 :: char3 :: Nil).stringify))
     case _ :: rest => filterNeededEntries2(rest, neededEntries)
   }
-
-  /**
-   * byr (Birth Year) - four digits; at least 1920 and at most 2002.
-   * iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-   * eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-   * hgt (Height) - a number followed by either cm or in:
-   * If cm, the number must be at least 150 and at most 193.
-   * If in, the number must be at least 59 and at most 76.
-   * hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-   * ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-   * pid (Passport ID) - a nine-digit number, including leading zeroes.
-   * cid (Country ID) - ignored, missing or not.
-   */
 
   implicit class asPasswordEntryValue(chars: List[Char]) {
     def isValid: Boolean = chars match {
@@ -80,6 +58,7 @@ object Day4 {
       case _ => false
     }
 
+    @tailrec
     private def isValidHgt(chars: List[Char], currentNumberString: String = ""): Boolean = chars match {
       case 'i' :: 'n' :: tail if tail.isEnd && currentNumberString.nonEmpty => currentNumberString.toInt >= 59 && currentNumberString.toInt <= 76
       case 'c' :: 'm' :: tail if tail.isEnd && currentNumberString.nonEmpty => currentNumberString.toInt >= 150 && currentNumberString.toInt <= 193
